5fad731f9cad77595e75b968303b841ff1eed34b
tkirby848 <53063062+tkirby848@users.noreply.github.com>
2021-07-06 17:25:23 -0500

    Update spoolerStatus.ps1

--- spoolerStatus.ps1
+++ spoolerStatus.ps1
@@ -1,13 +1,16 @@
- #run these two commands on a DC or a PC with RSAT installed
-#Import-Module ActiveDirectory
-#get-adcomputer -filter *  | select name > pcList.csv 
+#run these two commands on a DC or a PC with RSAT installed
+#1. Import-Module ActiveDirectory
+#2. get-adcomputer -filter *  | select name > pcList.csv 
 
 #set the path of the file with the domain listing
 $computers = Get-Content -Path .\pcList.csv
+
+#run through each item in the list
 $results = ForEach ($computer in $computers) 
 {
+#create a PS remote session to each computer and get the status of the Spooler Service
     $sess = New-PSSession -ComputerName $computer.Trim();
     Invoke-Command -ScriptBlock{get-Service Spooler} -Session $sess
 }
-
-$results > test.csv 
+#print the results out to a csv
+$results > results.csv 
